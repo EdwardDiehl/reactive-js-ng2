4fafd59744b0115e43bc1b50f2e015e70922ed2a
Edward Diehl <EdwDiehl@gmail.com>
2016-09-15 20:04:12 +0300

    Add SequenceService

---
+++ src/app/shared/sequence.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Observable } from  "rxjs/Observable";
+import "rxjs/add/observable/range";
+import "rxjs/add/operator/delay"
+import "rxjs/add/operator/repeat"
+
+@Injectable()
+export class SequenceService {
+  public sequence:Observable<number>;
+
+  constructor() {}
+
+  getSequence(begin:number = 1, end:number = 100, delay:number = 1000, isRepeatable:boolean = true):void {
+
+    this.sequence = Observable.range(begin, end)
+        .delay(delay);
+
+    if(isRepeatable) {
+      this.sequence = this.sequence.repeat()
+    }
+  }
+
+}
