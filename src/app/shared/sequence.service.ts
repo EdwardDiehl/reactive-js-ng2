b5f20a6459d9fcf2ee3cf44dbc877ff6f25bada6
Edward Diehl <EdwDiehl@gmail.com>
2016-09-16 20:15:37 +0300

    Add initial never completed observable to the SequenceService

--- src/app/shared/sequence.service.ts
+++ src/app/shared/sequence.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from  "rxjs/Observable";
 import "rxjs/add/observable/range";
+import "rxjs/add/observable/never";
 import "rxjs/add/operator/delay"
 import "rxjs/add/operator/repeat"
 
@@ -8,7 +9,9 @@ import "rxjs/add/operator/repeat"
 export class SequenceService {
   public sequence:Observable<number>;
 
-  constructor() {}
+  constructor() {
+    this.sequence = Observable.never();
+  }
 
   getSequence(begin:number = 1, end:number = 100, delay:number = 1000, isRepeatable:boolean = true):void {
 
